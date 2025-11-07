31a22401dc5b8d6400049fb9e4b436b306b427a6
Belen La Patrona <mariabelencaldez@gmail.com>
2025-11-06 23:15:33 -0300

    feat(repository): criar interface WeatherRepository extendendo JpaRepository

--- src/main/java/com/gntech/challenge/weatherapi/repository/WeatherRepository.java
+++ src/main/java/com/gntech/challenge/weatherapi/repository/WeatherRepository.java
@@ -1,4 +1,9 @@
 package com.gntech.challenge.weatherapi.repository;
 
-public class WeatherRepository {
+import com.gntech.challenge.weatherapi.entity.WeatherEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WeatherRepository extends JpaRepository<WeatherEntity, Long>{
 }
